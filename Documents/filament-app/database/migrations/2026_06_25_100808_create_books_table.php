b443802a97ec5b162206717c7dad1e816440f255
Neville <otiendeneville@gamil.com>
2026-06-25 13:11:37 +0300

    add title and author for the books

--- Documents/filament-app/database/migrations/2026_06_25_100808_create_books_table.php
+++ Documents/filament-app/database/migrations/2026_06_25_100808_create_books_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->string('author');
+            
         });
     }
 
