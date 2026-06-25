7b6856ea955e19b6f14811ea49cf227510d546e9
Neville <otiendeneville@gamil.com>
2026-06-25 13:17:20 +0300

    add protected fillabel

--- Documents/filament-app/app/Models/Book.php
+++ Documents/filament-app/app/Models/Book.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
-}
+    Protected $fillable =[
+       'title',
+       'author',
+       'image',
+       'description',
+    ];
+};
