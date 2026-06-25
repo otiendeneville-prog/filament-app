15e5e739b88a6736a81df0f4c784073f25755ff9
Neville <otiendeneville@gamil.com>
2026-06-25 13:13:09 +0300

    add image string and setting it to nullable

--- Documents/filament-app/database/migrations/2026_06_25_100808_create_books_table.php
+++ Documents/filament-app/database/migrations/2026_06_25_100808_create_books_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
             $table->timestamps();
             $table->string('title');
             $table->string('author');
-            
+            $table->string('image')->nullable();
+
         });
     }
 
