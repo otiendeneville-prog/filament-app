77210f79c9102f76e715f339704498b5a422e1c5
Neville <otiendeneville@gamil.com>
2026-06-25 13:14:25 +0300

    add table description

--- Documents/filament-app/database/migrations/2026_06_25_100808_create_books_table.php
+++ Documents/filament-app/database/migrations/2026_06_25_100808_create_books_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('author');
             $table->string('image')->nullable();
+            $table->string('description')->nullable();
 
         });
     }
