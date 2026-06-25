35de0ce3abba7c37ee93b7f77360e45e7bf0708d
Neville <otiendeneville@gamil.com>
2026-06-25 11:56:57 +0300

    add filament admin

--- Documents/filament-app/app/Providers/Filament/AdminPanelProvider.php
+++ Documents/filament-app/app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
             ])
