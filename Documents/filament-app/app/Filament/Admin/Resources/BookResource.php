bac92a8cf73a4c11d172157f3c6ba4bfb3ed79c8
Neville <otiendeneville@gamil.com>
2026-06-26 07:27:42 +0300

    importing textinput

--- Documents/filament-app/app/Filament/Admin/Resources/BookResource.php
+++ Documents/filament-app/app/Filament/Admin/Resources/BookResource.php
@@ -12,6 +12,8 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+
 
 class BookResource extends Resource
 {
