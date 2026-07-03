a1bbaf2e0d33c73555095344134a79e4db236517
Neville <otiendeneville@gamil.com>
2026-07-03 09:19:08 +0300

    generating a resource in the app pannel

---
+++ Documents/filament-app/app/Filament/App/Resources/BookResource/Pages/CreateBook.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\App\Resources\BookResource\Pages;
+
+use App\Filament\App\Resources\BookResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBook extends CreateRecord
+{
+    protected static string $resource = BookResource::class;
+}
