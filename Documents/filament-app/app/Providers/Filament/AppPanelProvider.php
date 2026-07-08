3e130872ce315a7adeddc749a6a251eee8a1b1e8
Neville <otiendeneville@gamil.com>
2026-07-08 10:36:48 +0300

    adding amber color

--- Documents/filament-app/app/Providers/Filament/AppPanelProvider.php
+++ Documents/filament-app/app/Providers/Filament/AppPanelProvider.php
@@ -36,8 +36,8 @@ class AppPanelProvider extends PanelProvider
             ->brandLogo('images/logo.png')
             ->brandLogoHeight('2.4rem')
             ->colors([
-                'primary' => 'oklc(0.778 0.175 124.8)',
-                'gray' =>Color::Slate,
+                'primary' => Color::Lime,
+                'gray' => Color::Slate,
             ])
             ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Admin\\Resources')
             
